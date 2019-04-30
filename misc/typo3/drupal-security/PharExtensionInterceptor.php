985a70f2c44e8b4300d15603c9d2689d7cda78f2
Ted Benice <ted@bythewaylabs.com>
2019-04-30 06:50:21 -0700

    move over core files from the aegir backup of platform

--- misc/typo3/drupal-security/PharExtensionInterceptor.php
+++ misc/typo3/drupal-security/PharExtensionInterceptor.php
@@ -22,6 +22,7 @@ class PharExtensionInterceptor implements Assertable {
    *
    * @param string $path
    *   The path of the phar file to check.
+   *
    * @param string $command
    *   The command being carried out.
    *
@@ -45,8 +46,6 @@ class PharExtensionInterceptor implements Assertable {
   }
 
   /**
-   * Determines if a path has a .phar extension or invoked execution.
-   *
    * @param string $path
    *   The path of the phar file to check.
    *
